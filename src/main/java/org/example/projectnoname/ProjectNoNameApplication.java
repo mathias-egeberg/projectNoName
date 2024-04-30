1965f1d451d26c661934a3779017d4f5a7366e30
mathias <mathias.egeberg@hotmail.no>
2024-04-30 08:35:45 +0200

    first commit

---
+++ src/main/java/org/example/projectnoname/ProjectNoNameApplication.java
@@ -0,0 +1,13 @@
+package org.example.projectnoname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNoNameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectNoNameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projectnoname/ProjectNoNameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projectnoname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNoNameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
